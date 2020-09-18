df002fbf624cacfa345a9aa06d9883177a9bca64
AmyKeedwell <amii1595@live.co.uk>
2020-09-18 11:22:46 +0000

    added card matching and timeout

--- scripts.js
+++ scripts.js
@@ -1,7 +1,28 @@
 const cards = document.querySelectorAll('.cards-medium');
 
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
 function flipCard() {
    this.classList.toggle('flip');
+
+   if(!hasFlippedCard) {
+       hasFlippedCard = true;
+       firstCard = this;
+   } else {
+       hasFlippedCard = false;
+       secondCard = this;
+
+       if(firstCard.dataset.name === secondCard.dataset.name){
+           firstCard.removeEventListener('click', flipCard);
+           secondCard.removeEventListener('click', flipCard);
+       } else {
+           setTimeout(() => {
+           firstCard.classList.remove('flip');
+           secondCard.classList.remove('flip');
+       }, 1500);
+   }
+}
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
