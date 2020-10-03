131d87dcb9a166288430d67e220a34ac7adfa33b
AmyKeedwell <amii1595@live.co.uk>
2020-10-03 12:40:47 +0000

    added basic 30 sec timer

--- scripts.js
+++ scripts.js
@@ -64,3 +64,11 @@ cards.forEach(card => card.addEventListener('click', flipCard));
 function refreshPage() {
   window.location.reload();
 }
+
+var seconds = document.getElementById("countdown").textContent;
+var countdown = setInterval(function(){
+    seconds--;
+    (seconds == 1) ? document.getElementById("plural").textContent = "" : document.getElementById("plural").textContent = "s";
+    document.getElementById("countdown").textContent = seconds;
+    if (seconds <= 0) clearInterval(countdown);
+},1000);
